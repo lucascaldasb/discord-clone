5fd93a9b5436051cd75c895d7cf7d8d83a17d4fa
lucascaldasb <lucas20caldas@gmail.com>
2023-09-08 15:49:03 -0300

    adding theme toggle
    Signed-off-by: lucascaldasb <lucas20caldas@gmail.com>

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
-      Protected route
+      <div className="w-10 h-10 rounded-full bg-indigo-500" />
+      <ModeToggle />
     </div>
   )
 }
